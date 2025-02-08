f33e45065c18caf4f6239bc08c6caeeca7d57a2a
VCT <VCT@DESKTOP-N7B6AUI>
2025-02-08 10:56:11 +0530

    Sharing FrameWork

---
+++ FrameWork/src/test/java/ContactTest/ToCreateContactTest.java
@@ -0,0 +1,8 @@
+package ContactTest;
+
+public class ToCreateContactTest {
+	 public static void main(String[] args) {
+			System.out.println("Contact created successfully");
+		}
+
+}
---
+++ FrameWork/src/test/java/OraganizationTest/ToCreateOraganization.java
@@ -0,0 +1,7 @@
+package OraganizationTest;
+
+public class ToCreateOraganization {
+	public static void main(String[] args) {
+		System.out.println("oraganization created successfully");
+	}
+}
