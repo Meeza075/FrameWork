501e819757d2dc7b646db89364b9e60dc200701f
Tilak <Tilak@LAPTOP-ME59NCKV>
2025-02-08 21:38:07 +0530

    Tilak commit

--- FrameWork/src/test/java/ContactTest/ToCreateContactTest.java
+++ FrameWork/src/test/java/ContactTest/ToCreateContactTest.java
@@ -3,6 +3,8 @@ package ContactTest;
 public class ToCreateContactTest {
 	 public static void main(String[] args) {
 			System.out.println("Contact created successfully");
+			
+			System.out.println("Tilak retuwn all the edited script");
 		}
 
 }
